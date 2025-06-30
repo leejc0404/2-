6ee0f45eafa08395ec7907f4d58890ddef030e8d
leejc0404 <126437838+leejc0404@users.noreply.github.com>
2025-07-01 02:33:42 +0900

    Update sso_quiz.py

--- sso_quiz.py
+++ sso_quiz.py
@@ -121,7 +121,7 @@ if quiz:
                     st.balloons()
                 elif quiz["start_hour"] == 14:
                     st.balloons()
-                elif quiz["start_hour"] == 16:
+                elif quiz["start_hour"] == 2:
                     st.markdown("""
                     <div style="position:relative;width:100%;height:0;padding-bottom:56.25%;">
                         <iframe src="https://giphy.com/embed/26ufdipQqU2lhNA4g" 
